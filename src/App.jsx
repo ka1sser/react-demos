4c4121bf361681dc6dcb75a09391770c4e7e6684
ka1sser <serenokaisser.dev@gmail.com>
2025-05-17 18:40:09 +0800

    Intro to what JSX is

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Greet from './components/Greet'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <Greet/>
-    </div>
+    <>
+      <div>
+        <Greet/>
+      </div>
+      <div>
+        <Header/>
+        <MainContent/>
+        <Footer/>
+      </div>
+    </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>This is how I am reborn.</p>
+        </main>
+    )
+}
+
+export default MainContent;
