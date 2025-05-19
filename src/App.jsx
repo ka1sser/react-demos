5e1bda0a866b4205c03135fb5d6e5c43f0c9dde2
ka1sser <serenokaisser.dev@gmail.com>
2025-05-19 14:16:30 +0800

    Using props.children to display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import Person from './components/Person';
-import Product from './components/Product';
+import Card from './components/Card';
 
 
 const App = () => {
 
   return (
     <>
-      <Person name="Kawanser" age={12} />
-      <Product prod_name="Scizor EX Special Illustration Rare" prod_price={50}/>
+      <Card>
+        <h1>React Developer</h1>
+        <h2>Proficient in React and React Native.</h2>
+      </Card>
+      <Card>
+        <h1>Game Developer</h1>
+        <h2>Proficient in Unity Game Engine and C# Scripting.</h2>
+      </Card>
+      <Card>
+        <h1>Human</h1>
+        <h2>Proficient in eating and sleeping.</h2>
+      </Card>
     </>
   )
 }
