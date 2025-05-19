93dfa2552e87390e804e8b08ef9177e1344d8ce1
ka1sser <serenokaisser.dev@gmail.com>
2025-05-19 16:45:41 +0800

    Added some linear gradient effect

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -3,7 +3,7 @@ import IconComponent from './IconComponent';
 
 const ProfileCard = ({card, rarity, price}) => {
     return(
-        <div className='bg-gray-300 p-[15px] rounded-[8px] text-black m-[10px]'>
+        <div className='bg-linear-to-r from-blue-600 to-cyan-200 p-[15px] rounded-[8px] text-black m-[10px]'>
             <div className='flex items-center space-x-2'>
                 <IconComponent isPerson={false}/>
                 <h1 className='text-2xl font-bold'>{card}</h1>
--- src/components/StyledCard.jsx
+++ src/components/StyledCard.jsx
@@ -3,7 +3,7 @@ import IconComponent from './IconComponent';
 
 const StyledCard = ({title,description,location}) => {
     return(
-        <div className="bg-blue-300 p-[20px] rounded-[10px] text-blue-950 m-[10px]">
+        <div className="bg-linear-to-r to-green-700 from-green-400 p-[20px] rounded-[10px] text-blue-950 m-[10px]">
             <div className='flex items-center space-x-2'>
                 <IconComponent isPerson={true}/>
                 <h1 className="text-2xl font-bold">{title}</h1>
