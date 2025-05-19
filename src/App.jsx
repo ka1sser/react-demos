f8db02a7cbf48d84e4ddda2268a836a017a0093a
ka1sser <serenokaisser.dev@gmail.com>
2025-05-19 10:06:59 +0800

    JSX continuation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import Greet from './components/Greet'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
 
 const App = () => {
   return (
     <>
-      <div>
-        <Greet/>
-      </div>
-      <div>
-        <Header/>
-        <MainContent/>
-        <Footer/>
-      </div>
+      <WelcomeMessage/>
     </>
   )
 }
---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+const WelcomeMessage = () => {
+    return(
+        <div>
+            <h1>Hello, People of the Earth!</h1>
+            <p>I will become a pro React Developer</p>
+        </div>
+    )
+}
+
+export default WelcomeMessage;
