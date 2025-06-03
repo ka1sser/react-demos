12685a65e5610b52ccdb0b3fa978550388743adf
ka1sser <serenokaisser.dev@gmail.com>
2025-06-03 11:13:54 +0800

    Introduction to useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./index.css";
-import CopyContent from './components/CopyContent';
 
 const App = () => {
 
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState(0);
+  const [something, setSomething] = useState(0);
+
+  useEffect(() => {
+    console.log("Called useEffect()")
+    document.title = `Increment ${value}`
+
+  },[value, something]);
 
   return (
-    <>
-      <CopyContent/>
-    </>
+    <div>
+      <h2>{value}</h2>
+      <button onClick={()=> setValue(value+1)}>Click Me</button>
+      <button onClick={()=> setSomething(value+1)}>Increment by Something</button>
+    </div>
   )
 }
 
