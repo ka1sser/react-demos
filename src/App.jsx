d672ba07d6d919108d2b5ce1b0cc38fe33fa1e56
ka1sser <serenokaisser.dev@gmail.com>
2025-06-03 11:15:32 +0800

    Using conditional in useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,18 @@ import "./index.css";
 const App = () => {
 
   const [value, setValue] = useState(0);
-  const [something, setSomething] = useState(0);
 
   useEffect(() => {
-    console.log("Called useEffect()")
-    document.title = `Increment ${value}`
-
-  },[value, something]);
+    if (value > 0) {
+      console.log("Called useEffect()")
+      document.title = `Increment ${value}`
+    }
+  },[value]);
 
   return (
     <div>
       <h2>{value}</h2>
       <button onClick={()=> setValue(value+1)}>Click Me</button>
-      <button onClick={()=> setSomething(value+1)}>Increment by Something</button>
     </div>
   )
 }
