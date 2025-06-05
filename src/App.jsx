39e11f77e30f96337479e1340e1e5ca50e9c8c52
ka1sser <serenokaisser.dev@gmail.com>
2025-06-05 10:14:38 +0800

    useRef introducton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Counter from "./components/Counter";
 import "./index.css";
+import { useRef } from "react";
 
 const App = () => {
+  const inputElement = useRef(null)
+  console.log(inputElement)
+
+  const focusInput = () => {
+    inputElement.current.focus();
+    inputElement.current.value = 'Kai';
+  }
+
   return ( 
     <div>
-      <Counter/>
+      <input type="text" ref={inputElement}/>
+      <button onClick={() => focusInput()}>FOCUS</button>
     </div>
   );
 };
